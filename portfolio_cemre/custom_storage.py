99e2f71c5b3c8248fa7f16b185223e5737b973dd
cemreyesil <cemreyesil243@gmail.com>
2025-03-03 13:11:24 +0200

    feat: update storage settings for local and S3 configurations

--- portfolio_cemre/custom_storage.py
+++ portfolio_cemre/custom_storage.py
@@ -1,17 +1,40 @@
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+
+if settings.DEBUG:
+    from django.core.files.storage import FileSystemStorage
 
 
-class MediaStorage(FileSystemStorage):
-    file_overwrite = False
-    default_acl = 'public-read'
+    class MediaStorage(FileSystemStorage):
+        file_overwrite = False
+        default_acl = 'public-read'
 
 
-class DocumentStorage(FileSystemStorage):
-    file_overwrite = False
-    default_acl = 'public-read'
+    class DocumentStorage(FileSystemStorage):
+        file_overwrite = False
+        default_acl = 'public-read'
 
 
-class ImageSettingStorage(FileSystemStorage):
-    file_overwrite = False
-    default_acl = 'public-read'
+    class ImageSettingStorage(FileSystemStorage):
+        file_overwrite = False
+        default_acl = 'public-read'
+
+else:
+    from storages.backends.s3boto3 import S3Boto3Storage
+
+
+    class MediaStorage(S3Boto3Storage):
+        location = 'media'
+        file_overwrite = False
+        default_acl = 'public-read'
+
+
+    class DocumentStorage(S3Boto3Storage):
+        location = 'media/documents'
+        file_overwrite = False
+        default_acl = 'public-read'
+
+
+    class ImageSettingStorage(S3Boto3Storage):
+        location = 'media/image_settings'
+        file_overwrite = False
+        default_acl = 'public-read'
--- portfolio_cemre/settings.py
+++ portfolio_cemre/settings.py
@@ -87,10 +87,7 @@ WSGI_APPLICATION = 'portfolio_cemre.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 # Password validation
@@ -129,13 +126,39 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+else:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+
+    DEFAULT_FILE_STORAGE = 'project_name.custom_storage.MediaStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    STATICFILES_LOCATION = 'static'
+    AWS_LOCATION = 'static'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    STATIC_ROOT = STATIC_URL
+
+    MEDIA_LOCATION = 'media'
+    IMAGE_SETTING_LOCATION = MEDIA_LOCATION + '/image_settings'
+    DOCUMENT_LOCATION = MEDIA_LOCATION + '/documents'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
