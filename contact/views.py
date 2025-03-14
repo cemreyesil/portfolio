4590eea757b204e21ca32ab70762e1d3768d800f
cemreyesil <cemreyesil243@gmail.com>
2025-03-14 22:45:26 +0200

    feat: add Message model and admin interface for contact form submissions

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from contact.models import Message
+
 
 # Create your views here.
 
@@ -9,8 +11,28 @@ def contact(request):
 
 
 def contact_form(request):
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        Message.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+        )
+
+        success = True
+        output = 'Contact form sent successfully.'
+
+    else:
+        success = False
+        output = 'Request method is not valid.'
+
     context = {
-        'success': True,
-        'message': 'Contact form sent successfully.'
+        'success': success,
+        'output': output,
     }
     return JsonResponse(context)
