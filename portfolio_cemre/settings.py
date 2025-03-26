4f427ee123d7744dca66d15f226b25fa474cea8b
cemreyesil <cemreyesil243@gmail.com>
2025-03-26 21:31:05 +0200

    feat: update Docker setup to use Gunicorn and configure environment variables for email and database

--- portfolio_cemre/settings.py
+++ portfolio_cemre/settings.py
@@ -166,6 +166,11 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Settings
-vars().update(env.email_url())
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
